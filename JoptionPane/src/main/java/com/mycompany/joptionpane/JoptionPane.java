8ddb687096d59b99f5429dd3c06d259e95da95d7
jmunozc023 <67330305+jmunozc023@users.noreply.github.com>
2022-08-01 18:53:34 -0600

    Ejercicio1

---
+++ JoptionPane/src/main/java/com/mycompany/joptionpane/JoptionPane.java
@@ -0,0 +1,9 @@
+package com.mycompany.joptionpane;
+import javax.swing.*;
+public class JoptionPane {
+    
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Un aviso");
+        System.out.println("Ya estas avisado");
+    }
+}
